dc3e79ec1dec203878ce0cbb75aa7ec3e61670c7
Alex <al.shmalts@yandex.ru>
2022-10-31 17:57:49 +0600

    Changed way to launch container, added git to custom image

--- DockerCreator.py
+++ DockerCreator.py
@@ -5,10 +5,10 @@ class DockerCreator:
 
 
     def run_jenkins_agent(self):
-        with open("/home/al/.ssh/jenkins_keys/id_rsa.pub", "r") as key_file:
-            ssh_key = key_file.readline(-1)
+        with open("/home/al/jenkins_learning/id_rsa.pub", "r") as key_file:
+            ssh_key = key_file.readline()
         subprocess.run("echo ssh key is " + ssh_key, shell=True)
-        return subprocess.run("docker run -d --rm --name jenkins_agent -p 4444:22 -e 'JENKINS_AGENT_SSH_PUBKEY=" + ssh_key.removesuffix("\n"), shell=True, capture_output=True)
+        return subprocess.run("docker run -d --rm --name jenkins_agent -p 4444:22 -e 'JENKINS_AGENT_SSH_PUBKEY=" + ssh_key.rstrip() + "' " + "jenkins_agent:2", shell=True, capture_output=True)
 
 
     def run_nginx_docker(self):
--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from DockerCreator import *
 dockerCreator = DockerCreator()
 
 print(dockerCreator.run_jenkins_agent())
-print(dockerCreator.run_nginx_docker())
 
